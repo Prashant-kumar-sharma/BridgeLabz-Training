cabcf6095a9e34206bf8ad7f9a806eced8981a90
Prashant-kumar-sharma <prashant10112003@gmail.com>
2025-12-17 12:55:46 +0530

    [Prashant] : added Find the age of Harry program

---
+++ gcr-codebase/programming-elements/level-1/FindAge.java
@@ -0,0 +1,13 @@
+// Writing Java Code to take Harry birth year as input and find his age.
+public class FindAge {
+	public static void main(String args[]) {
+		//create variable birthYear and currentYear to store Harry birth year and current year.
+		int birthYear = 2000, currentYear = 2024;
+		
+		//Create a variable age to calculate Harry's age 
+		int age = 2024 - birthYear;
+		
+		//Print Harry age
+		System.out.println("Harry's age in 2024 is " + age);
+	}
+}
