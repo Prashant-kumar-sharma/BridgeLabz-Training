d65a64544ce8372228e1a04c03c9528d34457a61
Prashant-kumar-sharma <prashant10112003@gmail.com>
2026-01-03 15:40:22 +0530

    [Prashant] : Updated the code with new logic

--- java-oops-practice/scenario-based-codebase/JavaOopsScenarios/src/com/day1/vehiclerentalapplication/Main.java
+++ java-oops-practice/scenario-based-codebase/JavaOopsScenarios/src/com/day1/vehiclerentalapplication/Main.java
@@ -5,40 +5,69 @@ import java.util.Scanner;
 class Main {
 
 	public static void displayMenu() {
-		
+
 		System.out.println("-------------------------------------------------------");
 		System.out.println("|     Welcome to Prashant Vehicle Rental Centre       |");
 		System.out.println("-------------------------------------------------------");
 		System.out.println();
 	}
-    public static void main(String[] args) {
 
-    	Scanner input =new Scanner(System.in);
-        Customer customer = new Customer();
+	public static void main(String[] args) {
 
-        customer.setName("Prashant Kumar Sharma");
+		Scanner input = new Scanner(System.in);
+		Customer customer = new Customer();
 
-        Vehicle[] vehicles = {
-            new Bike("BIKE101", " Royal Enfield Classic 350", 800),
-            new Car("CAR101", "Tata Nexon", 1000),
-            new Truck("TRUCK101", "Mahindra Jeeto", 2500)
-        };
-        
-        displayMenu();
-        System.out.print("Enter the number of days to rent the vehicle: ");
-        int days = input.nextInt();
+		customer.setName("Prashant Kumar Sharma");
 
-        System.out.println("\nCustomer : " + customer.getName());
-        System.out.println("\n--------------------------------\n");
+		Vehicle[] vehicles = { new Bike("BIKE101", " Royal Enfield Classic 350", 800),
+				new Car("CAR101", "Tata Nexon", 1000), new Truck("TRUCK101", "Mahindra Jeeto", 2500) };
 
-        for (Vehicle v : vehicles) {
+		displayMenu();
+		System.out.print("Enter the number of days to rent the vehicle: ");
+		int days = input.nextInt();
 
-            v.displayDetails();
-            System.out.println("Rental Days : " + days);
-            System.out.println("Total Rent  : Rs. " + v.calculateRent(days));
-            System.out.println("\n--------------------------------\n");
-        }
-        
-        input.close();
-    }
+		System.out.println("\nCustomer : " + customer.getName());
+		System.out.println("\n--------------------------------\n");
+
+		System.out.println("Vehicles List");
+		System.out.println("--------------------------------\n");
+		for (Vehicle v : vehicles) {
+
+			v.displayDetails();
+			System.out.println("\n--------------------------------\n");
+		}
+
+		System.out.println("Enter the Vehicle to Rent (Bike, Car, Truck) :");
+		String choice = input.next().trim().toLowerCase();
+		switch (choice) {
+		case "bike":
+			System.out.println("Vehicle Choosed : Bike");
+			vehicles[0].displayDetails();
+			System.out.println("Total Rent  : Rs. " + vehicles[0].calculateRent(days));
+			System.out.println("\n--------------------------------\n");
+			break;
+
+		case "car":
+			System.out.println("Vehicle Choosed : Car");
+			vehicles[1].displayDetails();
+			System.out.println("Surcharge : Rs. 500");
+			System.out.println("Total Rent  : Rs. " + vehicles[1].calculateRent(days));
+			System.out.println("\n--------------------------------\n");
+			break;
+
+		case "truck":
+			System.out.println("Vehicle Choosed : Truck");
+			vehicles[2].displayDetails();
+			System.out.println("Surcharge : Rs. 1000");
+			System.out.println("Total Rent  : Rs. " + vehicles[2].calculateRent(days));
+			System.out.println("\n--------------------------------\n");
+			break;
+
+		default:
+
+			System.out.println("Invalid Input! Exitting the System");
+		}
+
+		input.close();
+	}
 }
